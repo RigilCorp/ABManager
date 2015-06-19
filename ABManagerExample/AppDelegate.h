2bba8acd78dd23ba6b88187c3d0c0f1d9c0ecb4e
Sean <sean.rada@gmail.com>
2015-06-19 14:23:47 -0400

    Initial Commit

---
+++ ABManagerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ABManagerExample
+//
+//  Created by Sean Rada on 6/19/15.
+//  Copyright (c) 2015 Rigil Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ABManagerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ABManagerExample
+//
+//  Created by Sean Rada on 6/19/15.
+//  Copyright (c) 2015 Rigil Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
